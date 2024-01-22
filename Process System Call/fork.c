f00de37f57d3461e38b77f2f2cd4e10becf3f31b
Manoj Kumar BV <135972453+drakewolf007@users.noreply.github.com>
2024-01-22 19:06:25 +0430

    Create fork.c

---
+++ Process System Call/fork.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[]){
+	int id,wt;
+	id = fork();
+	if(id == 0){
+		printf("child - My ID is : %d\n",getpid());
+		printf("Child - My parent ID is : %d\n",getppid());
+		execl("./app","app","argc","argv[1]",NULL);
+		exit(34);
+	}
+	else if(id > 0){
+		wait(&wt);
+		printf("Parent - My ID is : %d\n",getpid());
+		printf("Parent - My parent ID is : %d\n",getppid());
+
+		if(WIFEXITED(wt)){
+			printf("The child terminated normally and status is : %d\n", WEXITSTATUS(wt));
+		}
+		else if(WIFSIGNALED(wt)){
+			printf("The child terminated abnormally and is killed by signal %d\n", WTERMSIG(wt));
+		}
+	}
+}
+
+/*Application
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char str[100];
+	printf("Enter the string : ");
+	scanf("%s",str);
+	int len = strlen(str);
+	printf("The origianl string is : %s\n",str
+	for(int i = 0,j=len-1; i <= j; i++,j--){
+		char c = str[i];
+		str[i] = str[j];
+		str[j] = c;
+	}
+	printf("The reversed  string is  : %s\n",str);
+	
+	return 0;
+}
+*/
