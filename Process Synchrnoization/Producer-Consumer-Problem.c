1c57fea37cebb634b20c6a3e8b318d537918ccba
Manoj Kumar BV <135972453+ManojKumarBVhi@users.noreply.github.com>
2024-03-10 23:56:31 +0630

    Update Producer-Consumer-Problem.c

--- Process Synchrnoization/Producer-Consumer-Problem.c
+++ Process Synchrnoization/Producer-Consumer-Problem.c
@@ -1,46 +1,50 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 #include<semaphore.h>
 
-int counter = 0;
-sem_t empty, full, mutex;
+int count = 0;
+sem_t empty,full,mutex;
 
-void * producer(void * args){
-  sem_wait(&empty);
-  sem_wait(&mutex);
-  printf("Producer process produced, process %d\n", ++counter);
-  sem_post(&mutex);
-  sem_post(&full);
+void *producer(void *args){
+	sem_wait(&empty);
+	sem_wait(&mutex);
+	printf("Producer produced %d process \n",++count);
+	sem_post(&mutex);
+	sem_post(&full);
 }
 
-void * consumer(void * args){
-  sem_wait(&full);
-  sem_wait(&mutex);
-  printf("Consumer process consumed, process %d\n", counter--);
-  sem_post(&mutex);
-  sem_post(&empty);
+void *consumer(void *args){
+	sem_wait(&full);
+	sem_wait(&mutex);
+	printf("Consumer consumed %d process \n", count--);
+	sem_post(&mutex);
+	sem_post(&empty);
 }
 
 int main(){
-  sem_init(&empty, 1 , 5);
-  sem_init(&full, 1, 0);
-  sem_init(&mutex, 1, 1);
-  int m,n;
-  printf("Enter the value of producer threads : ");
-  scanf("%d",&m);
-  printf("Enter the value of Consumer threads : ");
-  scanf("%d", &n);
-  pthread_t ptid[m];
-  for(int i=0; i<m; i++)
-      pthread_create(&ptid[i],NULL, producer, NULL);
-  for(int i=0; i<m; i++)
-      pthread_join(ptid[i],NULL);
-  
-  pthread_t tid[n];
-  for(int j=0; j<n; j++)
-      pthread_create(&tid[j], NULL, consumer, NULL);
-  for(int j=0; j<n; j++)
-      pthread_join(tid[j], NULL);
-
-  return 0;
+	sem_init(&mutex,0,1);
+	sem_init(&empty,0,5);
+	sem_init(&full,0,0);
+	
+	int m,n,i,j;
+	printf("Enter the producer values : ");
+	scanf("%d",&m);
+	
+	printf("Enter the consumer values : ");
+	scanf("%d",&n);
+	
+	pthread_t p[m];
+	for(i=0;i<m;i++)
+		pthread_create(&p[i],NULL,producer,NULL);
+	for(i=0;i<m;i++)
+	pthread_join(p[i],NULL);
+	
+	pthread_t c[n];
+	for(j=0;j<n;j++)
+		pthread_create(&c[j],NULL,consumer,NULL);
+	for(j=0;j<n;j++)
+		pthread_join(c[j],NULL);
+	
+	return 0;
 }
